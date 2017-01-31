0287b41926252e2dd790cf18e61064bc95b96493
syst3m_ERROR <shadowblade2014@gmail.com>
2017-01-31 15:52:43 -0500

    Completed Repairs page, added to

--- JS/scripts.js
+++ JS/scripts.js
@@ -56,3 +56,31 @@ function urlExists(url, callback){
     }
   });
 }
+
+
+
+    $(document).ready(function() {
+        // Add smooth scrolling to all links
+      //  var a = document.getElementById("anchor1");
+        $("#scroll, #scroll2").on('click', function(event) {
+
+            // Make sure this.hash has a value before overriding default behavior
+            if (this.hash !== "") {
+                // Prevent default anchor click behavior
+                event.preventDefault();
+
+                // Store hash
+                var hash = this.hash;
+
+                // Using jQuery's animate() method to add smooth page scroll
+                // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 800, function() {
+
+                    // Add hash (#) to URL when done scrolling (default click behavior)
+                    //window.location.hash = hash;
+                });
+            } // End if
+        });
+    });
