82325221e9ec0215be3a1e35cf3d79eb917ec354
syst3m_ERROR <shadowblade2014@gmail.com>
2017-02-20 16:12:32 -0500

    Major framework for generator
    added base structure for jquery.
    blog geneator first part done
    god im so tired

--- JS/generator.js
+++ JS/generator.js
@@ -1,6 +1,6 @@
 
 $(function () {
-  $('#sandbox-container .input-group.date').datepicker({
+  $('#sandbox-container input').datepicker({
   });
 });
 
@@ -36,12 +36,87 @@ $(document).ready(function(){
   });
 
   $("#submit-blog").click(function(){
-    console.log("OW BITCH");
-    if($("#blog-title").val() == "Sample Blog Title"){
-      console.log("Overwrite the defualt moron.");
+    var d = new Date();
+
+    var title = $("#blog-title").val();
+    var date = $('#sandbox-container .input-group.date').datepicker( 'getFormattedDate' );
+    var content = $("#content").val();
+
+    $("#blog-title").removeClass("incorrect-field");
+    $('#date-field').removeClass("caution-field");
+    $("#content").removeClass("incorrect-field");
+    $("#add-to-blog .copyable").empty();
+    $(".error").remove();
+    $(".caution").remove();
+
+    $("#add-to-blog").addClass("invisible");
+
+    var greenToGo = true;
+
+    if(title == "Sample Blog Title" || title === ""){
+      console.log("Overwrite the defualt");
+      greenToGo = false;
+      $("#blog-title").addClass("incorrect-field");
+      $("#add-to-index").addClass("invisible");
     }else{
-      console.log($("#blog-title").val());
+      console.log(title);
     }
+
+    if(date === ""){
+      console.log("I guess we're using today.");
+      var month = d.getMonth()+1;
+      var day = d.getDate();
+      date = (month<10 ? '0' : '') + month + '/' +    (day<10 ? '0' : '') + day + '/' + d.getFullYear();
+      console.log(date);
+      $('#date-field').addClass("caution-field");
+      $("<span class=\"caution\">If left blank the computer will assume a default value</span>").insertAfter(".caution-field");
+    }else{
+      console.log(date);
+    }
+
+    if(content === ""){
+      console.log("Overwrite the default");
+      $("#content").addClass("incorrect-field");
+      greenToGo = false;
+    }else{
+      content = cleanHTML(content);
+      console.log(content);
+    }
+
+    if(greenToGo){
+      console.log("here we go!");
+      //generate stuff
+      $("#add-to-blog").removeClass("invisible");
+      $("#add-to-index").removeClass("invisible");
+
+      $("#add-to-blog .copyable").append("&lt;div class=\"col-sm-6 move\"&gt;<br />");
+      $("#add-to-blog .copyable").append("   &lt;div class=\"panel panel-primary\"&gt;<br />");
+      $("#add-to-blog .copyable").append("    &lt;div class=\"panel-heading\"&gt;<br />");
+      $("#add-to-blog .copyable").append("      &lt;h3&gt;" + title + "&lt;/h3&gt;<br />");
+      $("#add-to-blog .copyable").append("      &lt;small&gt;" + date + "&lt;/small&gt;<br />");
+      $("#add-to-blog .copyable").append("    &lt;/div&gt;<br />");
+      $("#add-to-blog .copyable").append("    &lt;div class=\"panel-body\"&gt;<br />");
+      $("#add-to-blog .copyable").append("      " + content + "<br />");
+      $("#add-to-blog .copyable").append("    &lt;/div&gt;<br />");
+      $("#add-to-blog .copyable").append("  &lt;/div&gt;<br />");
+      $("#add-to-blog .copyable").append("&lt;/div&gt;<br />");
+
+
+
+    }else{
+      $("<span class=\"error\">Please overwrite the default value</span>").insertBefore(".incorrect-field");
+    }
+
+
+
   });
 
 });
+
+
+
+function cleanHTML(str){
+  str = str.replace(/</g, '&lt;');
+  str = str.replace(/>/g, '&gt;');
+  return str;
+}
